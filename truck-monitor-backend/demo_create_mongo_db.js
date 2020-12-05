c8c83cdcb85d9a8c5167bfc9f0482645358a1d46
nasrin khodayari <nasrin6890@yahoo.com>
2020-12-05 18:03:46 +0330

    Remove extra file

--- truck-monitor-backend/demo_create_mongo_db.js
+++
@@ -1,9 +0,0 @@
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://nasrin:<password>@cluster0.ruzih.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true ,useUnifiedTopology:true});
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
