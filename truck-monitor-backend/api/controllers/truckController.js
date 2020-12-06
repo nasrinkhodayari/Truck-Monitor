e2e1df6a3544a9ef086ce5b6d7c5d31f17097bd3
nasrin khodayari <nasrin6890@yahoo.com>
2020-12-06 13:25:43 +0330

    ADD:Add not found error to get truck api

--- truck-monitor-backend/api/controllers/truckController.js
+++ truck-monitor-backend/api/controllers/truckController.js
@@ -19,11 +19,27 @@ exports.create_one_or_bunch_truck = function (req, res) {
 };
 exports.read_a_truck = function (req, res) {
     Truck.find({ license_plate: req.params.license_plate }, function (err, truck) {
+        
         if (err)
             res.status(500).send({
                 message: err.message,
                 detail: err.detail
             });
-        res.json(truck);
+        if (truck.length !== 0) {
+            let searchResult = {
+                current_lat: truck[0].current_lat,
+                current_lng: truck[0].current_lng,
+                license_plate: truck[0].license_plate,
+                source_lat: truck[0].source_lat,
+                source_lng: truck[0].source_lng,
+                truckRoute: truck[0].truckRoute
+            };
+            res.json(searchResult);
+        }
+        else
+            res.status(404).send({
+                message: 'TrucK not found,Please insert correct LicensePlate.',
+                detail: 'Item not found'
+            });
     });
 };
--- truck-monitor-backend/server.js
+++ truck-monitor-backend/server.js
@@ -4,12 +4,18 @@ const express = require("express");
 const bodyParser = require('body-parser');
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }).
-then(() => console.log("connection established")).
-catch(error => console.log(`connection failed because =>${error}`));
+    then(() => console.log("connection established")).
+    catch(error => console.log(`connection failed because =>${error}`));
 
 require("./api/models/truckModel");
 const app = express();
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var truckRoute = require('./api/routes/truckRoute'); //importing route
 truckRoute(app); //register the route
