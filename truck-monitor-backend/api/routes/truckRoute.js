180b9d3c29a88ba324c5048d7ee8b890698821f8
nasrin khodayari <nasrin6890@yahoo.com>
2020-12-07 00:45:04 +0330

    FIX:remove extra  files

--- truck-monitor-backend/api/routes/truckRoute.js
+++ truck-monitor-backend/api/routes/truckRoute.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     let truckItem = require('../controllers/truckController');
-    app.route('/getTruck').get(truckItem.list_all_trucks);
+    app.route('/getAllTruck').get(truckItem.list_all_trucks);
     app.route('/addTruck').post(truckItem.create_one_or_bunch_truck);
     app.route('/getTruck/:license_plate').get(truckItem.read_a_truck);
 };
